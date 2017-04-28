e6cf1066b9200d66cc7fe8536a1b3873af25c0c9
Nick Jarin <email": null>
2017-04-28 14:48:31 -0700

    Remove js: true from Capybara test

--- spec/features/missions_spec.rb
+++ spec/features/missions_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Mission", type: :feature, js: true do
+RSpec.feature "Mission", type: :feature do
   scenario "renders Mission component" do
     visit "/"
     expect(page).to have_content "Less than 5% of cancer patients participate in clinical trials despite having a participant satisfaction rate of more than 90%."
