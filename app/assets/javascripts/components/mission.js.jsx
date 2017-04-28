a18ec671b93d282c699fa54c6058e54033cefc27
Nick Jarin <email": null>
2017-04-28 11:06:55 -0700

    Update mission statement copy

--- app/assets/javascripts/components/mission.js.jsx
+++ app/assets/javascripts/components/mission.js.jsx
@@ -1,15 +1,15 @@
 class Mission extends React.Component {
   render() {
     return(
-    <div className="our-mission col-xs-10 col-md-9">
-      <span>
-        <h2>Our Mission</h2>
-      </span>
-      <span>
-        <p>
-          Our mission is to connect patients with research, increase awareness and education, and provide clinical trial knowledge (and hope) for anyone diagnosed with cancer.
-        </p>
-      </span>
+    <div className="row">
+      <div className="our-mission col-xs-10 col-md-10 panel-default">
+        <div className="mission-header .panel-heading">
+          <h2 className=".panel-title">Our Mission</h2>
+        </div>
+        <span className="mission-content">
+            Less than 5% of cancer patients participate in clinical trials despite a 90% satisfaction rate among participants. Not enough patients (and their doctors) know what trials are available to them. Patients need more options. Researchers need more participants. CRConnect exists to make information on clinical trials more avaialable to the people who can be helped by them.
+        </span>
+      </div>
     </div>
   )
   }
