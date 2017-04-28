0dfab32ffad13bddc19c0f9eb3a32dffbbd03580
Nick Jarin <email": null>
2017-04-28 10:37:42 -0700

    Create Mission component

---
+++ app/assets/javascripts/components/mission.js.jsx
@@ -0,0 +1,10 @@
+<div className:"our-mission col-xs-10 col-md-9">
+  <span>
+    <h2>Our Mission</h2>
+  </span>
+  <span>
+    <p>
+      Our mission is to connect patients with research, increase awareness and education, and provide clinical trial knowledge (and hope) for anyone diagnosed with cancer.
+    </p>
+  </span>
+</div>
