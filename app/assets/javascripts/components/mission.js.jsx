4c13fb67908f1ebd4ca89c8bb249881825935714
Nick Jarin <email": null>
2017-04-28 10:43:41 -0700

    Renders Mission component on index html

--- app/assets/javascripts/components/mission.js.jsx
+++ app/assets/javascripts/components/mission.js.jsx
@@ -1,10 +1,16 @@
-<div className:"our-mission col-xs-10 col-md-9">
-  <span>
-    <h2>Our Mission</h2>
-  </span>
-  <span>
-    <p>
-      Our mission is to connect patients with research, increase awareness and education, and provide clinical trial knowledge (and hope) for anyone diagnosed with cancer.
-    </p>
-  </span>
-</div>
+class Mission extends React.Component {
+  render() {
+    return(
+    <div className="our-mission col-xs-10 col-md-9">
+      <span>
+        <h2>Our Mission</h2>
+      </span>
+      <span>
+        <p>
+          Our mission is to connect patients with research, increase awareness and education, and provide clinical trial knowledge (and hope) for anyone diagnosed with cancer.
+        </p>
+      </span>
+    </div>
+  )
+  }
+}
