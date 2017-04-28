13919ef8df2a29f0e05a78bcf188ea2b8bc72afa
Dan McKeon <email": null>
2017-04-28 11:36:19 -0700

    add two tests for trial model

--- spec/models/trial_spec.rb
+++ spec/models/trial_spec.rb
@@ -1,23 +1,14 @@
 require 'rails_helper'
 
-describe Trial do
-  describe 'instantiaion' do
-    let(:owner) {User.create(name: "Jorel", email: "jorel@gmail.com", password: "123", role: "owner")}
-    let(:staff1) {User.create(name: "Juan", email: "juan@gmail.com", password: "123", role: "staff")}
-    let(:staff2) {User.create(name: "April", email: "april@gmail.com", password: "123", role: "staff")}
-    let(:experiment) {Experiment.create(title: "Autogenics", summary: "Say the word, feel better", staff_needed: 5)}
+RSpec.describe Trial, type: :model do
+  let(:trial) { FactoryGirl.create(:trial) }
+  describe 'instatiation' do
+    it 'persists in database' do
+      expect(trial.created_at).not_to be_nil
+    end
 
-    it 'has users' do
-      experiment.users << [owner, staff1]
-      expect(experiment.users.length).to eq 2
-    end
-    it 'has an owner' do
-      experiment.users << owner
-      expect(experiment.owner).to eq owner
-    end
-    it 'has staff' do
-      experiment.users << [staff1, staff2]
-      expect(experiment.staff.length).to eq 2
+    it 'saves as "include" for squamous nsclc search' do
+      expect(trial.type_lung_nsclc_squamous).to eq 'include'
     end
   end
 end
