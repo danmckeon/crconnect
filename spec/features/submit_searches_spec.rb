ba26a0bbb873dd10e0e3caead804e99235301aa4
Nick Jarin <email": null>
2017-04-29 17:55:04 -0700

    Update feature test because we changed the view it was expecting

--- spec/features/submit_searches_spec.rb
+++ spec/features/submit_searches_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "SubmitSearches", type: :feature, js: true do
   scenario "render results page on search submit" do
     visit('/')
     click_on('Find Trials')
-    expect(page).to have_content('Results')
+    expect(page).to have_content('Showing clinical trials for')
   end
 end
