33b9561f138c8bb9756304e57636b6b185d4eff7
Dan McKeon <email": null>
2017-04-30 13:46:14 -0700

    convert error messages to p tags instead of ul

--- app/assets/javascripts/components/errors.js.jsx
+++ app/assets/javascripts/components/errors.js.jsx
@@ -1,12 +1,10 @@
 class Errors extends React.Component {
   render () {
     const errors = Object.keys(this.props.errors).map((field) =>
-      <li key={field} className="alert alert-danger">{this.props.errors[field]}</li>
+      <p key={field} className="alert alert-danger">{this.props.errors[field]}</p>
     );
     return(
-      <div className="errors">
-        <ul>{errors}</ul>
-      </div>
+      <div className="errors">{errors}</div>
     )
   }
 }
