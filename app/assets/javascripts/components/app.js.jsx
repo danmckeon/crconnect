0e1f2de9b5ec847907914a84478a28deecebb643
Dan McKeon <email": null>
2017-04-30 16:53:09 -0700

    write disclaimer

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -22,6 +22,11 @@ class App extends React.Component {
   render () {
     return(
       <div className="app">
+        {alert("This website is a prototype. It is intended to show information \
+pertaining to clinical trials relevant to a particular condition. We have made \
+our best effort in compiling this information but cannot guarantee the validity \
+of any information found on this site. Users should always consult their doctor \
+before acting on any information found on this website.")}
         <NavBar onClick={(e) => this.clickHandler(e)} />
         {(this.state.query) ? <Results query={this.state.query}/> : <Search onSubmit={(query) => this.submitHandler(query)} />}
       </div>
