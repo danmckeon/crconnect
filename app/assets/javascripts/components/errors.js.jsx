02374d83ec98d71917e3690b9f236acb2eaac5df
Dan McKeon <email": null>
2017-04-30 12:37:11 -0700

    build out errorrs rendering

--- app/assets/javascripts/components/errors.js.jsx
+++ app/assets/javascripts/components/errors.js.jsx
@@ -1,8 +1,11 @@
 class Errors extends React.Component {
   render () {
+    const errors = this.props.errors.map((error) =>
+      <li key={error}>{error}</li>
+    );
     return(
       <div className="errors">
-        <p>{JSON.stringify(this.props.errors)}</p>
+        <ul>{errors}</ul>
       </div>
     )
   }
