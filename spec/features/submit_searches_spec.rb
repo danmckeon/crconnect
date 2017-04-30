d3aa7113f8d50f994ba66253d0afb3f4189b267b
Nick Jarin <email": null>
2017-04-30 16:12:17 -0700

    Fix controller logic so that tests pass

--- spec/features/submit_searches_spec.rb
+++ spec/features/submit_searches_spec.rb
@@ -6,4 +6,29 @@ RSpec.feature "SubmitSearches", type: :feature, js: true do
     click_on('Find Trials')
     expect(page).to have_content('Showing clinical trials for')
   end
+
+scenario "render some results when given good input" do
+  visit('/')
+  select('Lung Cancer', from: 'cancerType')
+  select('Non-Small Cell Lung Cancer (Large Cell)', from: 'cancerSubType')
+  select('Stage IV', from: 'cancerStage')
+  select('Yes', from: 'chemotherapy')
+  select('Yes', from: 'radiation')
+  fill_in('age', with: '16')
+  click_on('Find Trials')
+  expect(page).to have_content('Showing clinical trials for')
+end
+
+before(:each){ FactoryGirl.create(:happy_submit) }
+scenario "render the correct results when given good input" do
+  visit('/')
+  select('Lung Cancer', from: 'cancerType')
+  select('Non-Small Cell Lung Cancer (Adenocarcinoma)', from: 'cancerSubType')
+  select('Stage I', from: 'cancerStage')
+  select('No', from: 'chemotherapy')
+  select('No', from: 'radiation')
+  fill_in('age', with: '50')
+  click_on('Find Trials')
+  expect(page).to have_content("Randomized Phase II")
+end
 end
