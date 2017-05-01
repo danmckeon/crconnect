fd2d4939e6d77ca9ac648218b0aac13c98cea0b4
Nick Jarin <email": null>
2017-05-01 09:59:24 -0700

    Break up copy text into its own folder, make ct101 use react-bootstrap-rails gem

--- app/assets/javascripts/components/ct101.js.jsx
+++ app/assets/javascripts/components/ct101.js.jsx
@@ -8,9 +8,9 @@ class CT101 extends React.Component {
 
   copyText(activeTab) {
     if(activeTab === "q1"){
-                          return "When you participate in a cancer clinical trial, you are guaranteed to receive either the treatment under investigation or the best available standard treatment. Unlike clinical studies in other therapeutic areas, patients in cancer trials receive a sugar pill placebo only if no standard treatment exists. Clinical trials offer you the opportunity to receive new cancer therapies not otherwise available. These therapies represent the most cutting edge thinking among research oncologists. When you volunteer for a cancer clinical trial, clinical trials (sometimes called clinical research studies) evaluate new drugs the U.S. Food and Drug Administration (FDA) hasn’t yet approved or test new uses for already approved drugs. They also evaluate new combinations of existing drugs and treatments, and different dosages of approved drugs, or find the best time to begin using a particular drug or treatment. Cancer is one of the most active areas in medical research today. At any time, thousands of clinical trials are underway on new cancer drugs and treatments. There may be multiple trials available nearby."
+                          return "When you participate in a clinical trial for a cancer treatment, you are guaranteed to receive either the treatment under study or the best available standard treatment. Unlike clinical studies in other therapeutic areas, patients in cancer trials receive a sugar pill placebo only if no standard treatment exists. Clinical trials offer you the opportunity to receive new cancer therapies not otherwise available. These therapies represent the most cutting edge thinking among research oncologists. Cancer is one of the most active areas in medical research today. At any time, thousands of clinical trials are underway on new cancer drugs and treatments. There may be multiple trials available nearby."
     } else if(activeTab === "q2"){
-                                  return "Patients in cancer clinical trials also receive increased monitoring and attention from nurses and doctors due to being in the trial.  Other benefits to participating in a cancer clinical trial include: 1) You may receive access to cutting edge treatments that aren’t available anywhere else. 2) It offers another option if your cancer has become resistant to treatment or has reoccurred. 3) You can help further medical research not only for yourself but for other cancer patients as well."
+                                  return "Patients in cancer clinical trials receive increased monitoring and attention from nurses and doctors due to being in the trial.  Other benefits to participating in a cancer clinical trial include: 1) You may receive access to cutting edge treatments that aren’t available anywhere else. 2) It offers another option if your cancer has become resistant to treatment or has reoccurred. 3) You can help further medical research not only for yourself but for other cancer patients as well."
     } else if(activeTab === "q3") {
                                   return "All medical treatments carry risks, even those that have been used by thousands of patients.  Not all treatments prove to be better than the standard of care and may produce additional side effects.  Yet all clinical trials must meet rigorous guidelines designed to protect you.  You will be monitored very closely during the trial for any potential side effects or problems.  Also, the health care professional involved in the study will explain any known or anticipated risks to you before you commit to the trial.  This is part of the informed consent process."
     } else if(activeTab === "q4") {
@@ -35,67 +35,73 @@ class CT101 extends React.Component {
   }
 
   render () {
+    const Panel = ReactBootstrap.Panel
+    const Container = ReactBootstrap.Container;
+    const Tab = ReactBootstrap.Tab;
+    const Col = ReactBootstrap.Col;
+    const Row = ReactBootstrap.Row;
+    const Nav = ReactBootstrap.Nav;
+    const NavItem = ReactBootstrap.NavItem;
+    const Content = ReactBootstrap.Content;
+    const Pane = ReactBootstrap.Pane;
     return(
       <div className="row">
-        <div className="col-sm-3"></div>
-      	<div className="col-sm-6">
-          <div className="panel panel-default">
-            <div className="panel-heading">
-              <h3 className="panel-title">
-                Clinical Trials 101
-              </h3>
-            </div>
-            <div className="panel-body">
-              <Question display={this.headerText(this.state.activeTab)} />
-              <Answer display={this.copyText(this.state.activeTab)} />
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-3"></div>
-      </div>
-
-      // <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-      //   <Row className="clearfix">
-      //     <Col sm={4}>
-      //       <Nav bsStyle="pills" stacked>
-      //         <NavItem eventKey="first">
-      //           {this.headerText("q1")}
-      //         </NavItem>
-      //         <NavItem eventKey="second">
-      //           {this.headerText("q2")}
-      //         </NavItem>
-      //         <NavItem eventKey="second">
-      //           {this.headerText("q3")}
-      //         </NavItem>
-      //         <NavItem eventKey="second">
-      //           {this.headerText("q4")}
-      //         </NavItem>
-      //         <NavItem eventKey="second">
-      //           {this.headerText("q5")}
-      //         </NavItem>
-      //       </Nav>
-      //     </Col>
-      //     <Col sm={8}>
-      //       <Tab.Content animation>
-      //         <Tab.Pane eventKey="first">
-      //           Tab 1 content
-      //         </Tab.Pane>
-      //         <Tab.Pane eventKey="second">
-      //           Tab 2 content
-      //         </Tab.Pane>
-      //         <Tab.Pane eventKey="first">
-      //           Tab 3 content
-      //         </Tab.Pane>
-      //         <Tab.Pane eventKey="second">
-      //           Tab 4 content
-      //         </Tab.Pane>
-      //         <Tab.Pane eventKey="first">
-      //           Tab 5 content
-      //         </Tab.Pane>
-      //       </Tab.Content>
-      //     </Col>
-      //   </Row>
-      // </Tab.Container>
+         <div className="col-sm-3" />
+       	<div className="col-sm-6">
+           <div className="panel panel-default">
+             <div className="panel-heading">
+               <h3 className="panel-title">
+                 Clinical Trials 101
+               </h3>
+             </div>
+             <div className="panel-body">
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+        <Row className="clearfix">
+          <Col sm={4}>
+            <Nav bsStyle="pills" stacked>
+              <NavItem eventKey="first">
+                {this.headerText("q1")}
+              </NavItem>
+              <NavItem eventKey="second">
+                {this.headerText("q2")}
+              </NavItem>
+              <NavItem eventKey="third">
+                {this.headerText("q3")}
+              </NavItem>
+              <NavItem eventKey="fourth">
+                {this.headerText("q4")}
+              </NavItem>
+              <NavItem eventKey="fifth">
+                {this.headerText("q5")}
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col sm={8}>
+            <Tab.Content animation>
+              <Tab.Pane eventKey="first">
+                {this.copyText("q1")}
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">
+                {this.copyText("q2")}
+              </Tab.Pane>
+              <Tab.Pane eventKey="third">
+                {this.copyText("q3")}
+              </Tab.Pane>
+              <Tab.Pane eventKey="fourth">
+                {this.copyText("q4")}
+              </Tab.Pane>
+              <Tab.Pane eventKey="fifth">
+                {this.copyText("q5")}
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </div>
+         </div>
+       </div>
+       <div className="col-sm-3" />
+     </div>
     )
   }
 }
