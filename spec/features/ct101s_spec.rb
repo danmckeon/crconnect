b26aa61463b4471b4c388f892053077d7f5202af
Nick Jarin <email": null>
2017-05-01 11:24:40 -0700

    Add test for rendering CT101 content after toggle

--- spec/features/ct101s_spec.rb
+++ spec/features/ct101s_spec.rb
@@ -6,4 +6,10 @@ RSpec.feature "Clinical Trials 101", type: :feature, js: true do
     page.driver.browser.switch_to.alert.accept
     expect(page).to have_content("Clinical Trials 101")
   end
+  scenario "renders different answers after clicking a question" do
+    visit "/"
+    page.driver.browser.switch_to.alert.accept
+    click_on("Are clinical trials safe?")
+    expect(page).to have_content("You will be monitored very closely")
+  end
 end
