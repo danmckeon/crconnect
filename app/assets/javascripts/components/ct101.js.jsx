a2b1a0891a438be4ef878e791a9526b26d824182
Nick Jarin <email": null>
2017-04-30 20:06:53 -0700

    Add commented out skeleton of Bootstrap togglable tabs

--- app/assets/javascripts/components/ct101.js.jsx
+++ app/assets/javascripts/components/ct101.js.jsx
@@ -53,6 +53,49 @@ class CT101 extends React.Component {
         </div>
         <div className="col-sm-3"></div>
       </div>
+
+      // <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+      //   <Row className="clearfix">
+      //     <Col sm={4}>
+      //       <Nav bsStyle="pills" stacked>
+      //         <NavItem eventKey="first">
+      //           {this.headerText("q1")}
+      //         </NavItem>
+      //         <NavItem eventKey="second">
+      //           {this.headerText("q2")}
+      //         </NavItem>
+      //         <NavItem eventKey="second">
+      //           {this.headerText("q3")}
+      //         </NavItem>
+      //         <NavItem eventKey="second">
+      //           {this.headerText("q4")}
+      //         </NavItem>
+      //         <NavItem eventKey="second">
+      //           {this.headerText("q5")}
+      //         </NavItem>
+      //       </Nav>
+      //     </Col>
+      //     <Col sm={8}>
+      //       <Tab.Content animation>
+      //         <Tab.Pane eventKey="first">
+      //           Tab 1 content
+      //         </Tab.Pane>
+      //         <Tab.Pane eventKey="second">
+      //           Tab 2 content
+      //         </Tab.Pane>
+      //         <Tab.Pane eventKey="first">
+      //           Tab 3 content
+      //         </Tab.Pane>
+      //         <Tab.Pane eventKey="second">
+      //           Tab 4 content
+      //         </Tab.Pane>
+      //         <Tab.Pane eventKey="first">
+      //           Tab 5 content
+      //         </Tab.Pane>
+      //       </Tab.Content>
+      //     </Col>
+      //   </Row>
+      // </Tab.Container>
     )
   }
 }
