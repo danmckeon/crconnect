23850e79bd0c00b558e060e95cb58c26d0ef693a
Nick Jarin <email": null>
2017-05-01 11:31:57 -0700

    Remove unused components that used to be part of CT101

--- app/assets/javascripts/components/answer.js.jsx
+++
@@ -1,9 +0,0 @@
-class Answer extends React.Component {
-  render () {
-    return(
-      <div>
-        <p>{this.props.display}</p>
-      </div>
-    )
-  }
-}
--- app/assets/javascripts/components/question.js.jsx
+++
@@ -1,9 +0,0 @@
-class Question extends React.Component {
-  render () {
-    return(
-      <div>
-        <h4>{this.props.display}</h4>
-      </div>
-    )
-  }
-}
