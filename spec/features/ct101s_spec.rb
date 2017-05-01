21ec836a882975caa3305043619f37d62d2c930f
Nick Jarin <email": null>
2017-05-01 11:28:49 -0700

    Add consecutive clicking test, pass all tests

--- spec/features/ct101s_spec.rb
+++ spec/features/ct101s_spec.rb
@@ -12,4 +12,16 @@ RSpec.feature "Clinical Trials 101", type: :feature, js: true do
     click_on("Are clinical trials safe?")
     expect(page).to have_content("You will be monitored very closely")
   end
+  scenario "renders different answers after clicking a question consecutively" do
+    visit "/"
+    page.driver.browser.switch_to.alert.accept
+    click_on("Are clinical trials safe?")
+    expect(page).to have_content("You will be monitored very closely")
+    click_on("What is informed consent?")
+    expect(page).to have_content("Signing the informed consent form acknowledges")
+    click_on("Why participate in a clinical trial?")
+    expect(page).to have_content("You may receive access to cutting edge")
+    click_on("How do I participate in a clinical trial?")
+    expect(page).to have_content("consult your doctor to see if it is a good fit for you.")
+  end
 end
