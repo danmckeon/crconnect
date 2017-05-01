a1cd0c212f16784811b3afed452a52cb25108a66
Philip Cornman <pjc5108@gmail.com>
2017-05-01 12:30:29 -0700

    work on jump to search feature.

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -1,12 +1,8 @@
 class NavBar extends React.Component {
-  onClick(e) {
-    e.preventDefault();
-    this.props.onClick(e)
-  }
   render() {
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
-        <div className="col-md-6">
+        <div className="col-xs-6 col-sm-6">
 
           <div>
             <a className="navbar-brand" href="/">
@@ -14,11 +10,9 @@ class NavBar extends React.Component {
             </a>
           </div>
         </div>
-        <div className="col-md-6">
+        <div className="col-xs-6 col-sm-6">
           <div className="nav navbar-nav navbar-right">
-            <ul className="nav navbar-nav navbar-right">
-              <li><button type="button" id="search-button" className="btn btn-default navbar-btn" onClick={(e) => this.onClick(e)}>Search for Clinical Trials</button></li>
-            </ul>
+            <a type="button" id="search-button" className="btn btn-default navbar-btn" href="#jumpsearch">Search for Clinical Trials</a>
           </div>
         </div>
       </nav>
--- app/assets/javascripts/components/searchForm.js.jsx
+++ app/assets/javascripts/components/searchForm.js.jsx
@@ -66,66 +66,76 @@ class SearchForm extends React.Component {
   render() {
     return (
       <div className="row">
-        <div className="col-md-3">
+        <div className="col-sm-1">
         </div>
-        <div className="search-form-container col-md-6">
-          <Errors errors={this.state.errors} />
-          <form className="search-form form-group" onSubmit={(e) => this.onSubmit(e)}>
-            <div className="row">
-              <label htmlFor="cancerType">Cancer Type</label>
-              <select name="cancerType" className="form-control">
-                <option value="Blank">(Please select type)</option>
-                <option value="Lung&nbsp;Cancer">Lung Cancer</option>
-              </select>
-              <label htmlFor="cancerSubType">Sub-Type</label>
-              <select name="cancerSubType" className="form-control">
-                <option value="Blank">(Please select sub-type)</option>
-                <option value="Non-Small&nbsp;Cell&nbsp;Lung&nbsp;Cancer&nbsp;(Adenocarcinoma)">Non-Small Cell Lung Cancer (Adenocarcinoma)</option>
-                <option value="Non-Small&nbsp;Cell&nbsp;Lung&nbsp;Cancer&nbsp;(Large&nbsp;Cell)">Non-Small Cell Lung Cancer (Large Cell)</option>
-                <option value="Non-Small&nbsp;Cell&nbsp;Lung&nbsp;Cancer&nbsp;(Squamous)">Non-Small Cell Lung Cancer (Squamous)</option>
-                <option value="Small&nbsp;Cell&nbsp;Lung&nbsp;Cancer">Small Cell Lung Cancer</option>
-              </select>
-              <label htmlFor="cancerStage">Stage</label>
-              <select name="cancerStage" className="form-control">
-                <option value="Blank">(Please select stage)</option>
-                <option value="Stage&nbsp;I">Stage I</option>
-                <option value="Stage&nbsp;II">Stage II</option>
-                <option value="Stage&nbsp;III">Stage III</option>
-                <option value="Stage&nbsp;IV">Stage IV</option>
-              </select>
-              {/* Commented out a couple fields for MVP. We can re-implement later */}
-              {/* <label htmlFor="cancerStatus">Status</label>
-              <select name="cancerStatus" className="form-control">
-                <option value="status_newly_diagnosed">Newly Diagnosed</option>
-                <option value="status_relapse">Relapse</option>
-                <option value="status_refractory">Refractory</option>
-              </select>
-              <label htmlFor="geneticMarkers">Genetic Markers</label>
-              <select name="geneticMarkers" className="form-control">
-                <option value="none"></option>
-                <option value="marker_alk_oncogene">ALK Oncogene</option>
-                <option value="marker_egfr_mutation">EGFR Mutation</option>
-                <option value="marker_kras_mutation">KRAS Mutation</option>
-              </select> */}
-              <label htmlFor="chemotherapy">Is chemotherapy your main form of treatment?</label>
-              <select name="chemotherapy" className="form-control">
-                <option value="Blank">(Please select yes or no)</option>
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-              <label htmlFor="radiation">Have you received radiation as a treatment?</label>
-              <select name="radiation" className="form-control">
-                <option value="Blank">(Please select yes or no)</option>
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-              <label htmlFor="age">Age</label>
-              <input type="text" name="age" className="form-control" />
-              <input type="submit" id="search-query-submit" className="form-control" value="Find Trials" />
+        <div className="search-form-container col-sm-10">
+          <div className="panel panel-default">
+            <div id="jumpsearch"/>
+            <div className="panel-heading">
+              <h3 className="panel-title">
+                Search for Clinial Trials
+              </h3>
             </div>
-          </form>
+            <div className="panel-body search-form-body">
+              <Errors errors={this.state.errors} />
+              <form className="search-form form-group" onSubmit={(e) => this.onSubmit(e)}>
+                <div className="row">
+                  <label htmlFor="cancerType">Cancer Type</label>
+                  <select name="cancerType" className="form-control">
+                    <option value="Blank">(Please select type)</option>
+                    <option value="Lung&nbsp;Cancer">Lung Cancer</option>
+                  </select>
+                  <label htmlFor="cancerSubType">Sub-Type</label>
+                  <select name="cancerSubType" className="form-control">
+                    <option value="Blank">(Please select sub-type)</option>
+                    <option value="Non-Small&nbsp;Cell&nbsp;Lung&nbsp;Cancer&nbsp;(Adenocarcinoma)">Non-Small Cell Lung Cancer (Adenocarcinoma)</option>
+                    <option value="Non-Small&nbsp;Cell&nbsp;Lung&nbsp;Cancer&nbsp;(Large&nbsp;Cell)">Non-Small Cell Lung Cancer (Large Cell)</option>
+                    <option value="Non-Small&nbsp;Cell&nbsp;Lung&nbsp;Cancer&nbsp;(Squamous)">Non-Small Cell Lung Cancer (Squamous)</option>
+                    <option value="Small&nbsp;Cell&nbsp;Lung&nbsp;Cancer">Small Cell Lung Cancer</option>
+                  </select>
+                  <label htmlFor="cancerStage">Stage</label>
+                  <select name="cancerStage" className="form-control">
+                    <option value="Blank">(Please select stage)</option>
+                    <option value="Stage&nbsp;I">Stage I</option>
+                    <option value="Stage&nbsp;II">Stage II</option>
+                    <option value="Stage&nbsp;III">Stage III</option>
+                    <option value="Stage&nbsp;IV">Stage IV</option>
+                  </select>
+                  {/* Commented out a couple fields for MVP. We can re-implement later */}
+                  {/* <label htmlFor="cancerStatus">Status</label>
+                  <select name="cancerStatus" className="form-control">
+                    <option value="status_newly_diagnosed">Newly Diagnosed</option>
+                    <option value="status_relapse">Relapse</option>
+                    <option value="status_refractory">Refractory</option>
+                  </select>
+                  <label htmlFor="geneticMarkers">Genetic Markers</label>
+                  <select name="geneticMarkers" className="form-control">
+                    <option value="none"></option>
+                    <option value="marker_alk_oncogene">ALK Oncogene</option>
+                    <option value="marker_egfr_mutation">EGFR Mutation</option>
+                    <option value="marker_kras_mutation">KRAS Mutation</option>
+                  </select> */}
+                  <label htmlFor="chemotherapy">Is chemotherapy your main form of treatment?</label>
+                  <select name="chemotherapy" className="form-control">
+                    <option value="Blank">(Please select yes or no)</option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                  </select>
+                  <label htmlFor="radiation">Have you received radiation as a treatment?</label>
+                  <select name="radiation" className="form-control">
+                    <option value="Blank">(Please select yes or no)</option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                  </select>
+                  <label htmlFor="age">Age</label>
+                  <input type="text" name="age" className="form-control" />
+                  <input type="submit" id="search-query-submit" className="form-control" value="Find Trials" />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="col-md-3">
+        <div className="col-sm-1">
         </div>
       </div>
     )
