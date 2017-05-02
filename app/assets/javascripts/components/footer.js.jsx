7e80e3808fb29295b7b902052ced3679654a899d
Dan McKeon <email": null>
2017-05-02 11:06:33 -0700

    add content and format for footer

--- app/assets/javascripts/components/footer.js.jsx
+++ app/assets/javascripts/components/footer.js.jsx
@@ -1,9 +1,28 @@
 class Footer extends React.Component {
   render() {
     return (
-      <div>
-      </div>
+      <footer className="panel-footer">
+        <div className="footer-contact-container">
+          <span className="footer-contact-text">
+            Contact Us: <a href="mailto:info@crconnect.org" target="_top">info@crconnect.org</a>
+          </span>
+          <span className="footer-contact-separator">|</span>
+          <span className="footer-contact-text">
+            Join Our Project on <a href="https://github.com/danmckeon/crconnect" target="_top">Github</a> Github
+          </span>
+        </div>
+        <div className="footer-disclaimer-container row">
+          <div className="col-sm-1 col-md-3"></div>
+        	<div className="col-sm-10 col-md-6">
+            <p>Disclaimer: This website is a prototype. It is intended to show information
+            pertaining to clinical trials relevant to a particular condition. We have made
+            our best effort in compiling this information but cannot guarantee the validity
+            of any information found on this site. Users should always consult their doctor
+            before acting on any information found on this website.</p>
+          </div>
+          <div className="col-sm-1 col-md-3"></div>
+        </div>
+      </footer>
     )
   };
-
 };
