e8462ccf382b9ef92bbe95133ba36ad5b4c023bc
Dan McKeon <email": null>
2017-05-02 11:20:40 -0700

    add additional formatting for footer

--- app/assets/javascripts/components/footer.js.jsx
+++ app/assets/javascripts/components/footer.js.jsx
@@ -8,19 +8,19 @@ class Footer extends React.Component {
           </span>
           <span className="footer-contact-separator">|</span>
           <span className="footer-contact-text">
-            Join Our Project on <a href="https://github.com/danmckeon/crconnect" target="_top">Github</a> Github
+            Find our Project on <a href="https://github.com/danmckeon/crconnect" target="_blank">Github</a>
           </span>
         </div>
         <div className="footer-disclaimer-container row">
-          <div className="col-sm-1 col-md-3"></div>
-        	<div className="col-sm-10 col-md-6">
+          <div className="col-sm-2"></div>
+        	<div className="col-sm-8">
             <p>Disclaimer: This website is a prototype. It is intended to show information
             pertaining to clinical trials relevant to a particular condition. We have made
             our best effort in compiling this information but cannot guarantee the validity
             of any information found on this site. Users should always consult their doctor
             before acting on any information found on this website.</p>
           </div>
-          <div className="col-sm-1 col-md-3"></div>
+          <div className="col-sm-2"></div>
         </div>
       </footer>
     )
