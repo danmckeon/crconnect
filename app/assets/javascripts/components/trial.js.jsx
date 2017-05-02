3bd7467a24a9f5e6caf5c6916a2c20b4846e57d9
Nick Jarin <email": null>
2017-05-02 10:55:22 -0700

    Add div with additional info in Trial component

--- app/assets/javascripts/components/trial.js.jsx
+++ app/assets/javascripts/components/trial.js.jsx
@@ -3,6 +3,13 @@ class Trial extends React.Component {
     return(
         <td>
           <a href={"https://clinicaltrials.gov/ct2/show/" + this.props.trial.nct_id}>{this.props.trial.brief_title}</a>
+          <div>
+            <ul>
+              <li>
+                <a href={"https://clinicaltrials.gov/ct2/show/" + this.props.trial.nct_id}>More info</a>
+              </li>
+            </ul>
+          </div>
         </td>
     )
   }
