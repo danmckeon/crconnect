1752d17f20191785ac605e3244f07cdbb870b641
Dan McKeon <email": null>
2017-05-02 14:41:49 -0700

    update db schema with trial_id migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502213343) do
+ActiveRecord::Schema.define(version: 20170502213934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170502213343) do
     t.string   "investigator_role"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.integer  "trial_id"
   end
 
   create_table "trials", force: :cascade do |t|
