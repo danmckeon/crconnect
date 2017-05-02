f9747548e02769b525ea0b6ce1930698c7a7adac
Nick Jarin <email": null>
2017-05-02 12:36:50 -0700

    Add logic for conditionally displaying contact info

--- app/assets/javascripts/components/trial.js.jsx
+++ app/assets/javascripts/components/trial.js.jsx
@@ -8,25 +8,36 @@ class Trial extends React.Component {
     $(`#${this.props.trial.id}`).slideToggle()
   }
 
+  checkContact() {
+    const contactName = this.props.trial.overall_contact_name;
+    const contactPhone = this.props.trial.overall_contact_phone;
+    const contactEmail = this.props.trial.overall_contact_email;
+
+    if (contactName !== "") {
+      return (
+        <li>
+          Contact info: <br/>
+            {contactName} <br/>
+            {contactPhone}  {contactEmail}
+        </li>
+      )
+    }
+  }
+
   render() {
     const briefTitle = this.props.trial.brief_title;
     const studyId = this.props.trial.id;
     const recruitingStatus = this.props.trial.overall_status;
-    const contactName = this.props.trial.overall_contact_name;
-    const contactPhone = this.props.trial.overall_contact_phone;
-    const contactEmail = this.props.trial.overall_contact_email;
 
     return (
       <td>
         <a onClick={this._toggleDiv} className="trial-buttons" >{briefTitle}</a>
         <div className="trial-details" id={studyId}>
           <ul>
-            <li>Currently {recruitingStatus}</li>
             <li>
-              Contact info: <br/>
-                {contactName} <br/>
-                {contactPhone}  {contactEmail}
+              Currently {recruitingStatus}
             </li>
+            {this.checkContact()}
             <li>
               <a href={`https://clinicaltrials.gov/ct2/show/${this.props.trial.nct_id}`}>More info</a>
             </li>
