ec3f6c30196fdca3ff698cc6b314c98ffd6c1551
Nick Jarin <email": null>
2017-05-03 14:25:01 -0700

    Add SessionsController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: login_params[:email])
+    if user && user.authenticate(login_params[:password])
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+  def login_params
+    params.require(:session).permit(:email, :password)
+  end
+end
