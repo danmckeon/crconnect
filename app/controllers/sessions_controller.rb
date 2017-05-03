543685f9e5b0f52405aebe79c93ac44a6116a33c
Nick Jarin <email": null>
2017-05-03 14:31:12 -0700

    Set up routes and route /admin to login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to new_session_path
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   root 'index#index'
-
-  # resources :tests
-
-  resources :trials, only: [:index, :show]
-
   resources :index, only: :index
+  resources :trials, only: [:index, :show]
+  resources :users
+  
+  get '/admin', to: 'sessions#new'
+  resources :sessions, only: [:new, :create, :destroy]
 end
