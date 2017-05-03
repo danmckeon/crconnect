ac4f091313a33e843f66cd29daac6f37dd59ae92
Nick Jarin <email": null>
2017-05-03 16:27:25 -0700

    Add navbar styling, fix route for post-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(login_params[:password])
       session[:user_id] = user.id
     end
+    redirect_to admin_trials_pending_path
   end
 
   private
