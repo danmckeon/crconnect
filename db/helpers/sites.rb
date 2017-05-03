f274a30a1bc649aecaf9aa37d19eaeccd52f1ca8
Dan McKeon <email": null>
2017-05-03 14:34:06 -0700

    add functions for finding latitude and longitude from other records in db and exporting to csv

--- db/helpers/sites.rb
+++ db/helpers/sites.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 def find_lat_long(sites)
   sites.each do |site|
     if site.zip
@@ -14,20 +16,40 @@ def find_lat_long(sites)
   end
 end
 
-
-def find_lat_long_within_db(missing_lat_long_sites)
+def find_lat_long_for_many_sites(missing_lat_long_sites)
   missing_lat_long_sites.each do |missing_lat_long_site|
-    if missing_lat_long_site.zip
-      potential_matches = Site.where(zip: missing_lat_long_site.zip)
-      potential_matches.each do |potential_match|
-        if !potential_match.latitude.nan?
-          missing_lat_long_site.latitude = potential_match.latitude
-          missing_lat_long_site.longitude = potential_match.longitude
-          missing_lat_long_site.save
+    find_lat_long_for_individual_site(missing_lat_long_site)
+  end
+end
+
+def find_lat_long_for_individual_site(missing_lat_long_site)
+  if missing_lat_long_site.zip
+    potential_matches = Site.where(zip: missing_lat_long_site.zip)
+    potential_matches.each do |potential_match|
+      if !potential_match.latitude.nan?
+        missing_lat_long_site.latitude = potential_match.latitude
+        missing_lat_long_site.longitude = potential_match.longitude
+        missing_lat_long_site.save
+        return
+      end
+    end
+  end
+end
+
+def save_lat_long_to_csv(all_sites)
+  CSV.open(Rails.root.join('db', 'seed_data', 'all_sites.csv'), 'wb') do |csv|
+    csv << ["nct_id", "name", "city", "state", "zip", "country", "status",
+      "contact_name", "contact_phone", "contact_phone_ext", "contact_email",
+      "investigator_name", "investigator_role", "latitude", "longitude"]
+    if !site.latitude.nan?
+      all_sites.each do |site|
+        if site.trial
+          csv << [site.trial.nct_id, site.name, site.city, site.state, site.zip,
+            site.country, site.status, site.contact_name, site.contact_phone,
+            site.contact_phone_ext, site.contact_email, site.investigator_name,
+            site.investigator_role, site.latitude, site.longitude]
         end
       end
-    # elsif site.state
-    #   location_input = "#{site.city}, #{site.state}"
     end
   end
 end
