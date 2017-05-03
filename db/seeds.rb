1fbc2baf5ab971347529f98a7cd49745a4444c8f
Dan McKeon <email": null>
2017-05-02 17:10:17 -0700

    move trial parsing to csv loop instead of nokogiri ct.gov loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,8 +57,11 @@ def find_trial_sites(trial_xml)
     new_site.status = site.at("status").text if site.at("status")
     new_site.contact_name = site.at("contact/last_name").text if site.at("contact/last_name")
     new_site.contact_phone = site.at("contact/phone").text if site.at("contact/phone")
-    p site.at("investigator/last_name").text if site.at("investigator/last_name")
-    p site.at("investigator/role").text if site.at("investigator/role")
+    new_site.contact_phone_ext = site.at("contact/phone_ext").text if site.at("contact/phone_ext")
+    new_site.contact_email = site.at("contact/email").text if site.at("contact/email")
+    new_site.investigator_name = site.at("investigator/last_name").text if site.at("investigator/last_name")
+    new_site.investigator_role = site.at("investigator/role").text if site.at("investigator/role")
+    new_site.save
     sites << new_site
   end
   sites
@@ -113,43 +116,8 @@ def create_trial_from_xml(trial_xml)
   trial[:has_expanded_access] = trial_xml.xpath("//has_expanded_access").text
   trial[:condition_browse_mesh_term] = trial_xml.xpath("//condition_browse//mesh_term").text
   trial.save
-  # trial.sites =
-  find_trial_sites(trial_xml)
   trial
 end
-
-# USE EVENTUALLY FOR PARSING SITES
-
-# def parse_trial_sites(xml_file)
-#   @location_array = xml_file.xpath("//location").map { |location| location }
-#   @location_array.each do |entry|
-#     if (entry.at("country").text == "United States")
-#       @trial_site_record = TrialSite.new
-#       @trial_site_record[:site_name] = read_entry(entry, "name")
-#       @trial_site_record[:site_city] = read_entry(entry, "city")
-#       @trial_site_record[:site_state] = read_entry(entry, "state")
-#       @trial_site_record[:site_zip] = read_entry(entry, "zip")
-#       @trial_site_record[:site_county] = read_entry(entry, "country")
-#       @trial_site_record[:site_status] = read_entry(entry, "status")
-#       @trial_site_record.save
-#     end
-#   end
-# end
-
-# def read_entry(entry, tag)
-#   if entry.at(tag) != nil
-#     return entry.at(tag).text
-#   end
-# end
-
-# Populate trial sites
-
-#TARGET_FILE_PATH = '/Users/danmckeon/workspace/cancer_research_connect/lib/data/CTDOTGOV_UPLOAD_1476145970/XML/NCT02364999.xml'
-
-
-
-
-
 
 
 SHORT_SAMPLE_UPLOAD_URL = "https://clinicaltrials.gov/ct2/results?term=&recr=Recruiting&cntry1=NA%3AUS&cond=hand+cancer&studyxml=true"
@@ -177,13 +145,11 @@ create_upload_directories(parent_dir_path, zip_dir_path, xml_dir_path)
 
 # download zip file to directory and unzip to xmls
 
-# AAA: UNCOMMENT THESE TO RUN THE REAL THING!!!
-# download_zip_file(LUNG_OR_COLO_OPEN_UPLOAD_URL, zip_dir_path, xml_dir_path)
-# download_zip_file(LUNG_OR_COLO_ACTIVENR_UPLOAD_URL, zip_dir_path, xml_dir_path)
-
-download_zip_file(LONGER_SAMPLE_UPLOAD_URL, zip_dir_path, xml_dir_path)
+download_zip_file(LUNG_OR_COLO_OPEN_UPLOAD_URL, zip_dir_path, xml_dir_path)
+download_zip_file(LUNG_OR_COLO_ACTIVENR_UPLOAD_URL, zip_dir_path, xml_dir_path)
 
 Trial.destroy_all
+Site.destroy_all
 
 # parse xml files to Trial objects
 
@@ -197,10 +163,10 @@ lung_and_colo_csv = File.join(Rails.root, 'db', 'seed_data', 'lung_and_colo.csv'
 missing_trials = []
 
 CSV.foreach(lung_and_colo_csv, headers: true, encoding: 'BOM|UTF-8:UTF-8') do |row|
+  new_trial_path = xml_dir_path + (row[0] + '.xml')
   if row[1] == "1"
     trial = Trial.find_by(nct_id: row[0])
   else
-    new_trial_path = xml_dir_path + (row[0] + '.xml')
     if File.exist? File.expand_path new_trial_path
       xml_file_noko = Nokogiri::XML(File.open(new_trial_path))
       trial = create_trial_from_xml(xml_file_noko)
@@ -208,6 +174,8 @@ CSV.foreach(lung_and_colo_csv, headers: true, encoding: 'BOM|UTF-8:UTF-8') do |r
   end
   if trial
     trial.update_attributes(row.to_hash)
+    trial.sites = find_trial_sites(Nokogiri::XML(File.open(new_trial_path)))
+    trial.save
   else
     missing_trials << row[0]
   end
