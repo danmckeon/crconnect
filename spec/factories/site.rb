c06a41d7357f5020c162e93dbbdc147532274517
Dan McKeon <email": null>
2017-05-04 13:37:35 -0700

    add site factory

---
+++ spec/factories/site.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :site do
+    name "University of Wisconsin Hospital and Clinics"
+    city "Madison"
+    state "Wisconsin"
+    zip "53792"
+    country "United States"
+    status "Recruiting"
+    contact_name "Andrew J Tatar"
+    contact_phone "608-263-2901"
+    contact_email "atatar@wis.edu"
+    investigator_name "Dustin Deming, MD"
+    investigator_role "Principal Investigator"
+    latitude 43.0772547
+    longitude -89.430773
+  end
+end
