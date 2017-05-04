c8eac8ccdf046e9aa85a8ed37ed4b2c848fdf1bb
Dan McKeon <email": null>
2017-05-04 14:28:18 -0700

    write feature test for showing correct number of miles from user to site

--- spec/features/submit_searches_spec.rb
+++ spec/features/submit_searches_spec.rb
@@ -200,4 +200,45 @@ RSpec.feature "SubmitSearches", type: :feature, js: true do
     expect(page).to have_content('98104')
     expect(page).to have_content("Your search did not match any trials.")
   end
+
+  scenario "show correct distance from user to site and site contact info" do
+    excited_submit.sites << site
+    site.save
+    visit('/')
+    page.driver.browser.switch_to.alert.accept
+    select('Colorectal Cancer (Other types)', from: 'cancerType')
+    select('Stage III', from: 'cancerStage')
+    select('No', from: 'chemotherapy')
+    select('No', from: 'radiation')
+    select('KRAS Mutation', from: 'geneticMarkers')
+    fill_in('age', with: '50')
+    fill_in('zipcode', with: '98117')
+    click_on('Find Trials')
+    expect(page).to have_content("Fairly important study")
+    page.find(".trial-buttons").click
+    expect(page).to have_content("Closest Clinical Trial Site (1617.69 miles away)")
+    expect(page).to have_content("University of Wisconsin Hospital and Clinics")
+    expect(page).to have_content("Andrew J Tatar")
+    expect(page).to have_content("608-263-2901")
+    expect(page).to have_content("atatar@wis.edu")
+  end
+
+  scenario "show correct order of trials based on site location" do
+    submission.sites << site
+    site.save
+    grumpy_submit.sites << site2
+    site2.save
+    visit('/')
+    page.driver.browser.switch_to.alert.accept
+    select('Small Cell Lung Cancer', from: 'cancerType')
+    select('Stage III', from: 'cancerStage')
+    select('No', from: 'chemotherapy')
+    select('No', from: 'radiation')
+    select('None', from: 'geneticMarkers')
+    fill_in('age', with: '50')
+    fill_in('zipcode', with: '98117')
+    click_on('Find Trials')
+    expect(page).to have_content("Phase III Randomized Trial Comparing Overall")
+    expect(page).to have_content("Study of the effects of the Phase on the status")
+  end
 end
