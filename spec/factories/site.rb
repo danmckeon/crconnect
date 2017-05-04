cf408651a8c4251d64291994842c596335441093
Dan McKeon <email": null>
2017-05-04 14:18:32 -0700

    define second site factory

--- spec/factories/site.rb
+++ spec/factories/site.rb
@@ -12,6 +12,20 @@ FactoryGirl.define do
     investigator_name "Dustin Deming, MD"
     investigator_role "Principal Investigator"
     latitude 43.0772547
-    longitude -89.430773
+    longitude (-89.430773)
+  end
+
+  factory(:site2, class: Site) do
+    name "Seattle Cancer Care Alliance"
+    city "Seattle"
+    state "Washington"
+    zip "98109"
+    country "United States"
+    status "Recruiting"
+    contact_name "Dean"
+    contact_phone "206-555-2324"
+    contact_email "dean@scca.org"
+    latitude 47.6288591
+    longitude (-122.3456919)
   end
 end
