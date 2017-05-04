deeebc9678db965506dc18984dd88489d88807f1
Philip Cornman <pjc5108@gmail.com>
2017-05-04 14:43:57 -0700

    reduce margin and padding more on alert containers, centered alerts to their parent container, removed bullet points.

--- app/assets/javascripts/components/errors.js.jsx
+++ app/assets/javascripts/components/errors.js.jsx
@@ -1,7 +1,7 @@
 class Errors extends React.Component {
   render () {
     const errors = Object.keys(this.props.errors).map((field) =>
-      <p key={field} className="alert alert-danger">&#8226; {this.props.errors[field]}</p>
+      <p key={field} className="alert alert-danger">{this.props.errors[field]}</p>
     );
     return(
       <div className="errors">{errors}</div>
