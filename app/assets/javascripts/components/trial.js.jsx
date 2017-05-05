6f2b9eb39f2707cfb02f862431c64243dee15c8e
Dan McKeon <email": null>
2017-05-04 19:11:17 -0700

    add breathing space to result bullets

--- app/assets/javascripts/components/trial.js.jsx
+++ app/assets/javascripts/components/trial.js.jsx
@@ -12,7 +12,7 @@ class Trial extends React.Component {
 
     if (site.contact_name) {
       return (
-        <li>
+        <li className="trial-details-bullets">
           Contact Info: <br/>
             {site.contact_name} <br/>
             {site.contact_phone} <br />
@@ -31,25 +31,25 @@ class Trial extends React.Component {
     const truncatedTitle = trial.brief_title.substring(0, 100) + '...'
 
     return (
-      <td>
+      <td className="trial-data">
         <div onClick={this._toggleDiv} className="trial-buttons" >
-          <a>Location: {site.name}<br />
-          Trial: {truncatedTitle} </a>
+          <a><strong>Location:</strong> {site.name}<br /><br />
+          <strong>Trial:</strong> {truncatedTitle} </a>
         </div>
         <div className="trial-details" id={studyId}>
-          <ul>
-            <li>
+          <ul className="trial-details-list">
+            <li className="trial-details-bullets">
               Currently {site.status}
             </li>
-            <li>
+            <li className="trial-details-bullets">
               Closest Clinical Trial Site ({distance.toFixed(2)} miles away): <br />
               {site.name} <br />
               {site.city}, {site.state} {site.zip}
             </li>
 
             {this.checkContact(site)}
-            <li>
-              <a href={`https://clinicaltrials.gov/ct2/show/${this.props.trialSite.trial.nct_id}`}>More info</a>
+            <li className="trial-details-bullets">
+              <a href={`https://clinicaltrials.gov/ct2/show/${this.props.trialSite.trial.nct_id}`} target="_blank">More info</a>
             </li>
           </ul>
         </div>
