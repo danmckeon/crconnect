ccd5ee9ca0e929769bdab07014517cd0ae3aa228
Philip Cornman <pjc5108@gmail.com>
2017-05-05 06:13:50 -0700

    center search button in nav bar.

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -5,20 +5,11 @@ class NavBar extends React.Component {
   }
   render() {
     return (
-      <nav className="navbar navbar-default navbar-fixed-top">
-        <div className="col-md-6">
-
-          <div>
-            <a className="navbar-brand" href="/">
+      <nav className="navbar navbar-default navbar-fixed-top navi">
+            <a href="/">
               <img id="top-logo" alt="Brand" src="crc_logo.png" className="img-responsive"/>
             </a>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="nav navbar-nav navbar-right">
             <button type="button" id="search-button" className="btn btn-default navbar-btn" onClick={(e) => this.onClick(e)}>Search for<br></br> Clinical Trials</button>
-          </div>
-        </div>
       </nav>
     )
   };
